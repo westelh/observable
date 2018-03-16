c2252ab8a7bcb8da6a0b17302ff0689045602448
westelh <reo.retriever@gmail.com>
2018-03-16 20:42:46 +0900

    refactor: Change observable value name

--- src/observable/headers/observable.hpp
+++ src/observable/headers/observable.hpp
@@ -3,7 +3,7 @@
 
 template <class T>
 class observable {
-        T value;
+        T value_m;
     public:
         observable();
         observable(const T&);
@@ -13,17 +13,17 @@ class observable {
 
 
 template <class T>
-observable<T>::observable(): value{} {  }
+observable<T>::observable(): value_m{} {  }
 
 template <class T>
-observable<T>::observable(const T& t): value{t} {  }
+observable<T>::observable(const T& t): value_m{t} {  }
 
 template <class T>
-observable<T>::observable(T&& t): value{std::forward<T>(t)} {  }
+observable<T>::observable(T&& t): value_m{std::forward<T>(t)} {  }
 
 template <class T>
 T observable<T>::get() const noexcept {
-    return value;
+    return value_m;
 }
 
 #endif
